63ab52c21727fc4cb4528038ff980248c2b5988d
johncollinsai <climatemetrics@gmail.com>
2023-03-18 17:32:30 +0800

    v0.1.12, more data preparation

--- volgpt_data.py
+++ volgpt_data.py
@@ -18,11 +18,6 @@ def high_frequency_data():
     # Set column headings
     df_data.columns = ["Date","Ticker","TimeBarStart","OpenBarTime","OpenBidPrice","OpenBidSize", "OpenAskPrice","OpenAskSize","FirstTradeTime","FirstTradePrice","FirstTradeSize","HighBidTime","HighBidPrice","HighBidSize","HighAskTime","HighAskPrice","HighAskSize","HighTradeTime","HighTradePrice","HighTradeSize","LowBidTime","LowBidPrice","LowBidSize","LowAskTime","LowAskPrice","LowAskSize","LowTradeTime","LowTradePrice","LowTradeSize","CloseBarTime","CloseBidPrice","CloseBidSize","CloseAskPrice","CloseAskSize","LastTradeTime","LastTradePrice","LastTradeSize","MinSpread","MaxSpread","CancelSize","VolumeWeightPrice","NBBOQuoteCount","TradeAtBid","TradeAtBidMid","TradeAtMid","TradeAtMidAsk","TradeAtAsk","TradeAtCrossOrLocked","Volume","TotalTrades","FinraVolume","FinraVolumeWeightPrice","UptickVolume","DowntickVolume","RepeatUptickVolume","RepeatDowntickVolume","UnknownTickVolume","TradeToMidVolWeight","TradeToMidVolWeightRelative","TimeWeightBid","TimeWeightAsk"]
 
-    # Set a date-time index, using OpenBarTime
-    df_data['DateTimeIndex'] = pd.to_datetime(df_data['Date'].astype(str)) + pd.to_timedelta(df_data['OpenBarTime'].astype(str))
-    df_data = df_data.set_index('DateTimeIndex')
-    df_data = df_data.drop(['Date','TimeBarStart'], axis=1) # Drop original Date and TimeBarStart columns
-
     # Calculate the proportion of missing values and zero values for each column
     prop_missing = (df_data.isna().sum() + (df_data == 0).sum()) / len(df_data)
     prop_missing_pct = prop_missing.map('{:.4%}'.format)  # Format as a percentage to 4dp
@@ -33,18 +28,20 @@ def high_frequency_data():
     df_data['CloseBidPrice'] = df_data['CloseBidPrice'].astype(float)
     df_data['CloseAskPrice'] = df_data['CloseAskPrice'].astype(float)
 
+    # Reduce number of columns for tractability
+    # NB: it may be helpful to use the other columns in the future to explore whether more data can be used to improve the model
+    df_data_full = df_data.copy()
+    df_data = df_data_full[['Ticker','CloseBidSize','CloseAskSize','CloseBidPrice','CloseAskPrice']].copy()   
+    df_data['DateTime'] = pd.to_datetime(df_data_full['Date'].astype(str)) + pd.to_timedelta(df_data_full['OpenBarTime'].astype(str))
+    
     # Compute WeightedMidPrice using the closing prices per analysis in my high-frequency-data post
     df_data['WeightedMidPrice'] = ((df_data['CloseBidSize']*df_data['CloseAskPrice']) + (df_data['CloseAskSize']*df_data['CloseBidPrice'])) / (df_data['CloseBidSize'] + df_data['CloseAskSize'])
 
     # Raw returns
     AAPL_rr = df_data.loc[df_data['Ticker'] == "AAPL"]
     AAPL_rr = AAPL_rr['WeightedMidPrice'] - AAPL_rr['WeightedMidPrice'].shift(1)
-    AAPL_rr = AAPL_rr[AAPL_rr.notna()].copy()
-    AAPL_rr = AAPL_rr[AAPL_rr != 0].copy()
     JPM_rr = df_data.loc[df_data['Ticker'] == "JPM"]
     JPM_rr = JPM_rr['WeightedMidPrice'] - JPM_rr['WeightedMidPrice'].shift(1)
-    JPM_rr = JPM_rr[JPM_rr.notna()].copy()
-    JPM_rr = JPM_rr[JPM_rr != 0].copy()
 
     # Merge AAPL_rr and JPM_rr with df_data
     df_data = df_data.merge(AAPL_rr.to_frame(name='AAPL_rr'), left_index=True, right_index=True, how='left')
@@ -54,23 +51,25 @@ def high_frequency_data():
     AAPL_lr = df_data.loc[df_data['Ticker'] == "AAPL"]
     AAPL_lr = np.log(AAPL_lr['WeightedMidPrice'].astype(float))
     AAPL_lr = AAPL_lr - AAPL_lr.shift(1)
-    AAPL_lr = AAPL_lr[AAPL_lr.notna()].copy()
-    AAPL_lr = AAPL_lr[AAPL_lr != 0].copy()
     JPM_lr = df_data.loc[df_data['Ticker'] == "JPM"]
     JPM_lr = np.log(JPM_lr['WeightedMidPrice'].astype(float))
     JPM_lr = JPM_lr - JPM_lr.shift(1)
-    JPM_lr = JPM_lr[JPM_lr.notna()].copy()
-    JPM_lr = JPM_lr[JPM_lr != 0].copy()
 
     # Append log returns as additional columns to df_data
     df_data = pd.concat([df_data, AAPL_lr.rename('AAPL_lr'), JPM_lr.rename('JPM_lr')], axis=1)
 
-    # Descriptive statistics
-    AAPL_stats = stats.describe(AAPL_rr)
-    JPM_stats = stats.describe(JPM_rr)
+    df_data.fillna("UNK", inplace=True) # Replace missing values with "UNK"
 
-    # returns plots see volgpt-data.py
+    df_data = df_data.astype(str) # Convert all columns to string
 
+    # Descriptive stats based upon raw returns
+    AAPL_rr_stat = AAPL_rr[AAPL_rr.notna()].copy() # raw returns for AAPL using df_data
+    AAPL_rr_stat = AAPL_rr_stat[AAPL_rr_stat != 0].copy()
+    JPM_rr_stat = JPM_rr[JPM_rr.notna()].copy() # raw returns for JPM using df_data
+    JPM_rr_stat = JPM_rr_stat[JPM_rr_stat != 0].copy()
+    AAPL_stats = stats.describe(AAPL_rr_stat) # Descriptive statistics for AAPL
+    JPM_stats = stats.describe(JPM_rr_stat) # Descriptive statistics for JPM
+    
     return (df_data, prop_missing_pct, AAPL_rr, JPM_rr, AAPL_lr, JPM_lr, AAPL_stats, JPM_stats)
 
 
